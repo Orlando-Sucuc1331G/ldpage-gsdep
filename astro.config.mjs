8c8f45e873d0deb3ba2258a268f282af4571fc46
Orlando-Sucuc1331G <orlando131app@gmail.com>
2026-05-29 00:45:39 -0600

    Remove deprecated github-pages integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github-pages';
 
 export default defineConfig({
   site: 'https://orlando-sucuc1331g.github.io',
   base: '/ldpage-gsdep',
-  integrations: [github()],
 });
